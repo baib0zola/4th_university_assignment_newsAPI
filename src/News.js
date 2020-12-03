e272ec93daf6f0bfbf0abfd2d9962493dc19a5d2
baib0zola <73535599+baib0zola@users.noreply.github.com>
2020-12-03 16:04:13 +0200

    Update News.js

--- src/News.js
+++ src/News.js
@@ -1,5 +1,5 @@
 const url =
-  '';
+  'http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=24ecceb71360404aa7c4a32034a7e31d';
 
 export async function getNews() {
   let result = await fetch(url).then((response) => response.json());
