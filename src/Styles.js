e91653c1711cd412d68c942f256f71a2b5a03965
baib0zola <73535599+baib0zola@users.noreply.github.com>
2020-12-04 11:44:07 +0200

    Update Styles.js

--- src/Styles.js
+++ src/Styles.js
@@ -1,7 +1,36 @@
-const url =
-  'http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=24ecceb71360404aa7c4a32034a7e31d';
+import { StyleSheet, Text } from 'react-native';
 
-export async function getNews() {
-  let result = await fetch(url).then((response) => response.json());
-  return result.articles;
-}
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#17202a',
+  },
+  mainCard: {
+    backgroundColor: '#212f3d',
+  },
+  desc: {
+    backgroundColor: '#212f3d',
+    marginBottom: 10,
+    color: '#f7f9f9',
+  },
+  div: {
+    backgroundColor: '#dfe6e9',
+  },
+  textBox: {
+    backgroundColor: '#212f3d',
+    flexDirection: 'row-reverse',
+  },
+  noteStyle: {
+    margin: 5,
+    fontStyle: 'italic',
+    color: '#f7f9f9',
+    fontSize: 10,
+  },
+  titleStyle: {
+    color: '#f7f9f9',
+    fontWeight: 'bold',
+    fontSize: 25,
+    fontStyle: 'italic',
+  },
+});
+
+export default styles;
